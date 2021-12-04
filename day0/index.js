599d8695739917bc94293b6a034155e7a56de5b8
Eli Morris-Heft <eli.morris.heft@gmail.com>
2021-12-04 12:56:46 -0500

    Make day0/ a better template for new days

--- day0/index.js
+++ day0/index.js
@@ -1,36 +1,11 @@
-// This is actually Day 1 from 2020
-
 function part1(input) {
-  const entries = input.trim().split('\n').map(x => +x);
-
-  // find the two entries that sum to 2020
-  let i, j;
-  outside: for(i = 0; i < entries.length; ++i) {
-    for(j = i + 1; j < entries.length; ++j) {
-      if(entries[i] + entries[j] === 2020) {
-        break outside;
-      }
-    }
-  }
-
-  return entries[i] * entries[j];
+  const lines = input.trim().split('\n');
+  return null;
 }
 
 function part2(input) {
-  const entries = input.trim().split('\n').map(x => +x);
-
-  // find the two entries that sum to 2020
-  let i, j, k;
-  outside: for(i = 0; i < entries.length; ++i) {
-    for(j = i + 1; j < entries.length; ++j) {
-      for(k = j + 1; k < entries.length; ++k)
-      if(entries[i] + entries[j] + entries[k] === 2020) {
-        break outside;
-      }
-    }
-  }
-
-  return entries[i] * entries[j] * entries[k];
+  const lines = input.trim().split('\n');
+  return null;
 }
 
 export default [ part1, part2 ];
